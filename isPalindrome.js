48cc77bfa8caec3dbaff597b0da1abb0bd98514a
Eddy Yun <71950315+eyun1988@users.noreply.github.com>
2022-06-22 09:17:16 -0700

    opt palindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,19 +1,44 @@
+// naive approach
+// var isPalindrome = (s) => {
+//     const parsedS = s.toLowerCase().replace(/[\W_]+/g, "");
+//     let left = 0,
+//         right = parsedS.length - 1;
+//     if (!parsedS) return true;
+//     while (left < right) {
+//         if (parsedS[left] !== parsedS[right]) {
+//             return false;
+//         }
+//         left++;
+//         right--;
+//     }
+//     return true;
+// };
+
+// less space, same time
 var isPalindrome = (s) => {
-    const parsedS = s.toLowerCase().replace(/[\W_]+/g, "");
     let left = 0,
-        right = parsedS.length - 1;
-    if (!parsedS) return true;
+        right = s.length - 1;
     while (left < right) {
-        if (parsedS[left] !== parsedS[right]) {
-            return false;
-        }
+        while (left < right && !alphaNum(s[left])) left++;
+        while (left < right && !alphaNum(s[right])) right--;
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
         left++;
         right--;
     }
     return true;
 };
 
-// console.log(isPalindrome("A man, a plan, a canal: Panama"));
+const alphaNum = (char) => {
+    const code = char.charCodeAt(0);
+    return (
+        (code > 47 && code < 58) ||
+        (code > 64 && code < 91) ||
+        (code > 96 && code < 123)
+    );
+    // return char.charCodeAt(0);
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
 // console.log(isPalindrome("race a car"));
 // console.log(isPalindrome(" "));
-console.log(isPalindrome("abb"));
+// console.log(isPalindrome("abb"));
